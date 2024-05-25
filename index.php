a07a71e7ae3727232276292eeb284ba8801d1289
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-05-25 17:48:40 +0100

    Learning

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php include './inc/header.php'; ?>
+<?php
+$name = $email = $body = '';
+$nameErr = $emailErr = $bodyErr = '';
+
+if (isset($_POST['submit'])) {
+    if (empty($_POST['name'])) {
+        $nameErr = 'Name is required';
+    }
+}
+?>
 <img src="./img/logo.png" class="w-25 mb-3" alt="">
 <h2>Feedback</h2>
 <p class="lead text-center">Leave feedback for Traversy Media</p>
-<form action="" class="mt-4 w-75">
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="mt-4 w-75" method="post">
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
         <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
